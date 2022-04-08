d20ddc16e414074fef52fc9ccf2e1c219cd4eda6
holgerpart <Hobevalge1>
2022-04-08 16:19:10 +0300

    HOLGER created CustomerService class, moved bankRepository object to MyAppApplication class

---
+++ src/main/java/ee/bcs/myApp/bank/CustomerService.java
@@ -0,0 +1,23 @@
+package ee.bcs.myApp.bank;
+
+import ee.bcs.myApp.MyAppApplication;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    public Integer addNewCustomer( CustomerDto customerDto) {
+        Customer customer = new Customer();
+        customer.setFirstName(customerDto.getFirstName());
+        customer.setLastName(customerDto.getLastName());
+        customer.setIsikukood(customerDto.getIsikukood());
+        customer.updateId();
+
+        List<Customer> customers = MyAppApplication.bankRepository.getCustomers();
+        customers.add(customer);
+
+        return customer.getId();
+    }
+}
