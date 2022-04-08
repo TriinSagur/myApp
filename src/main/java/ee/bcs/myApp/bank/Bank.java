f3f5cb782744ac773f3bab74447a945922febc59
kasparpoldemaa <kasparpoldemaa@gmail.com>
2022-04-08 16:19:14 +0300

    created some new endpoints

---
+++ src/main/java/ee/bcs/myApp/bank/Bank.java
@@ -0,0 +1,18 @@
+package ee.bcs.myApp.bank;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class Bank {
+
+    private List<Customer> customers = new ArrayList<>();
+    private List<Account> accounts = new ArrayList<>();
+    private List<Transaction> transactions = new ArrayList<>();
+
+
+
+
+}
