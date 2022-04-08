edf88c4ce95293cde803be78d7d7989c344f12d7
Kirillp1988 <kirillp1988@gmail.com>
2022-04-08 16:19:13 +0300

    KIRILL Created new endpoints

---
+++ src/main/java/ee/bcs/myApp/bank/Customer.java
@@ -0,0 +1,18 @@
+package ee.bcs.myApp.bank;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+
+    public static Integer idCounter = 1;
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String isikukood;
+
+    public void updateId() {
+        id = idCounter;
+        idCounter++;
+    }
+}
