46dc045db6af48d733b4fc8aa16c8aa25a96e5f4
marjutamm <tamm.marju@gmail.com>
2022-04-11 10:12:37 +0300

    MARJU created some new endpoints

--- src/main/java/ee/bcs/myApp/bank/CustomerController.java
+++ src/main/java/ee/bcs/myApp/bank/CustomerController.java
@@ -1,42 +1,36 @@
 package ee.bcs.myApp.bank;
 
+import ee.bcs.myApp.MyAppApplication;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
 
-    public static Bank bankRepository = new Bank();
+    @Resource
+    private CustomerService customerService;
 
-    @PostMapping("/add")
+    @PostMapping
     @Operation(summary = "Lisab uue kliendi.")
     public Integer addNewCustomer(@RequestBody CustomerDto customerDto) {
-        Customer customer = new Customer();
-        customer.setFirstName(customerDto.getFirstName());
-        customer.setLastName(customerDto.getLastName());
-        customer.setIsikukood(customerDto.getIsikukood());
-
-        customer.updateId();
-        List<Customer> customers = bankRepository.getCustomers();
-        customers.add(customer);
-
-        return customer.getId();
+        return customerService.addNewCustomer(customerDto);
     }
 
     @GetMapping("/all")
     @Operation(summary = "Tagastab kõik kliendid.")
     public List<Customer> getAllCustomers() {
 
-        return bankRepository.getCustomers();
+        return MyAppApplication.bankRepository.getCustomers();
     }
 
-    @GetMapping("/find")
+    @GetMapping("/id")
     @Operation(summary = "Leiab andmebaasi ID järgi kliendi")
     public Customer findCustomerById(@RequestParam Integer id) {
-        List<Customer> customers = bankRepository.getCustomers();
+        List<Customer> customers = MyAppApplication.bankRepository.getCustomers();
 
         Customer result = new Customer();
 
@@ -49,9 +43,9 @@ public class CustomerController {
         return result;
 
     }
-    @DeleteMapping("/remove")
+    @DeleteMapping("/id")
     public void removeCustomerId(Integer id) {
-        List<Customer> customers = bankRepository.getCustomers();
+        List<Customer> customers = MyAppApplication.bankRepository.getCustomers();
 
         Customer result = new Customer();
         for (Customer customer : customers) {
