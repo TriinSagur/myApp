344869c98a8f5ce8e025612781892cc59a982e76
ReimoM <reimosalo@gmail.com>
2022-04-11 14:26:35 +0300

    REIMO updated BookDto

--- src/main/java/ee/bcs/myApp/library/BookDto.java
+++ src/main/java/ee/bcs/myApp/library/BookDto.java
@@ -2,9 +2,13 @@ package ee.bcs.myApp.library;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class BookDto {
+    @NotNull
     private String title;
+    @NotNull
     private Integer year;
 
 }
