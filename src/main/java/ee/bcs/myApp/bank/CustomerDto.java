a98b1049177bc9eb863fbd1f733850fc7f812e3a
KarliEST <karlikullasepp@gmail.com>
2022-04-11 11:48:00 +0300

    CustomerControlleri, CustomerService ja CustomerDto muutmine

--- src/main/java/ee/bcs/myApp/bank/CustomerDto.java
+++ src/main/java/ee/bcs/myApp/bank/CustomerDto.java
@@ -3,6 +3,7 @@ package ee.bcs.myApp.bank;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class CustomerDto {
@@ -14,5 +15,6 @@ public class CustomerDto {
     private String lastName;
 
     @NotNull
+    @Size(min = 11,max = 11)
     private String isikukood;
 }
