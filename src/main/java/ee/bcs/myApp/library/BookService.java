11e24184cf02a3d1cbd41c58d57543cc4405e72e
500 <git.hub@myrk.eu>
2022-04-11 15:22:04 +0300

    JAAN created book and Library

---
+++ src/main/java/ee/bcs/myApp/library/BookService.java
@@ -0,0 +1,22 @@
+package ee.bcs.myApp.library;
+
+import ee.bcs.myApp.MyAppApplication;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    public Integer addNewBook(BookDto bookDto) {
+        Book book = new Book();
+
+        book.setTitle(bookDto.getTitle());
+        book.setYear(bookDto.getYear());
+        book.updateId();
+
+        List<Book> books = MyAppApplication.libraryRepository.getBooks();
+        books.add(book);
+        return book.getId();
+    }
+}
