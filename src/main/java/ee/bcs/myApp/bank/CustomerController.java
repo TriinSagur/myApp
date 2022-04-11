5fa0a89361d8d9f6925a07f03fc3805db955ab9c
MarliisRos <marliis17@gmail.com>
2022-04-11 11:46:31 +0300

    MARLIIS updated

--- src/main/java/ee/bcs/myApp/bank/CustomerController.java
+++ src/main/java/ee/bcs/myApp/bank/CustomerController.java
@@ -1,8 +1,11 @@
 package ee.bcs.myApp.bank;
 
+import ee.bcs.myApp.MyAppApplication;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -10,73 +13,37 @@ import java.util.List;
 
 public class CustomerController {
 
-    public static Bank bankRepository = new Bank();
+    @Resource
+    private CustomerService customerService;
 
-    // UUe kliendi lisamine, Satatic on kõikides klassides olemas. Kõigi objektide vahel jagatud.
-    // max 3 muutujat meetodis. 4 sissetulevat muutujat on juba plaju, hais tuleb juurde.
-    // päris elus teeksime selleks uue klassi uue kliendi lisamiseks.
 
-    @PostMapping("/add")
+    @PostMapping
     @Operation(summary= "lisab uue kliendi")
-    public Integer addNewCustomer(@RequestBody CustomerDto customerDto) {
-
-        // Mapime customeri dto  väljad ümer customeri klassi
-        //MAPstarct mäpib automaatselt ära need väljad mis on samade nimedega, erisusi kirjeldame annutatsioonidega.
-
-        Customer customer = new Customer();
-        customer.setFirstName(customerDto.getFirstName());
-        customer.setLastName(customerDto.getLastName());
-        customer.setIsikukood(customerDto.getIsikukood());
-        customer.updateId();
-
-        List<Customer> customers = bankRepository.getCustomers();
-        customers.add(customer);
-
-
-        return customer.getId();
-
+    public Integer addNewCustomer(@Valid @RequestBody CustomerDto customerDto) {
+        return customerService.addNewCustomer((customerDto));
     }
-
-    // list kõikidest klientidest, kes meil on. rada võiks olla nii selge kui saab olla.
-    // vaata uue klassi loomise videot youtube
 
     @GetMapping("/all")
     @Operation(summary = "tagastab kõik kliendid")
     public List<Customer> getAllCustomers() {
-        return bankRepository.getCustomers();
-
+        return customerService.getAllCustomers();
     }
-    // ennem kutsume andmed välja ja siis teeme kustutamise
-    // get eeldab, et midagi on olemas, find otsib kas on olemas
-    // vaata video for List LOOP
 
-    @GetMapping("/find")
+    @GetMapping("/id")
     @Operation (summary = "leiab andmebaasi ID järgi kliendikoodi")
     public Customer findCustomerById(@RequestParam Integer id) {
-        List<Customer> customers= bankRepository.getCustomers();
-
-        Customer result = new Customer();
-
-        for(Customer customer : customers) {
-            if (customer.getId().equals(id)) {
-                result = customer;
-
-            }
-        }
-        return result;
+        return customerService.findCustomerById(id);
     }
-    @DeleteMapping("/remove")
+
+    @DeleteMapping("/id")
+    @Operation (summary = "kustutab andmebaasi ID järgi kliendikoodi")
     public void removeCustomerId(@RequestParam Integer id) {
-        List <Customer> customers= bankRepository.getCustomers();
-
-        Customer result = new Customer();
-        for (Customer customer : customers) {
-            if (customer.getId().equals(id)) {
-                result = customer;
-            }
-        }
-
-        customers.remove(result);
-
+        customerService.findCustomerById(id);
     }
+    @PutMapping("/id")
+    @Operation (summary = "uuendab andmebaasi id järgi klienti.")
+    public void updateCustomerById(@RequestParam Integer id, @Valid @RequestBody CustomerDto customerDto) {
+        customerService.updateCustomerById(id, customerDto);
+    }
+
 }
--- src/main/java/ee/bcs/myApp/bank/CustomerDto.java
+++ src/main/java/ee/bcs/myApp/bank/CustomerDto.java
@@ -2,15 +2,24 @@ package ee.bcs.myApp.bank;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+// selle klassi teeme ainult andemete liigutamiseks
+
 @Data
 
 public class CustomerDto {
-    // selle klassi teeme ainult andemete liigutamiseks
 
+    @NotNull
     private String firstName;
-    private String lastName;
-    private String isikukood;
 
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    @Size(min = 11, max = 11)
+    private String isikukood;
 
 }
 
