cfa9de3f2fc879a85e0909d37331889a8a267ac1
holgerpart <Hobevalge1>
2022-04-14 16:17:53 +0300

    HOLGER updated

---
+++ src/main/java/ee/bcs/myApp/library/book/BookRepository.java
@@ -0,0 +1,10 @@
+package ee.bcs.myApp.library.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    @Query("select b from Book b where upper(b.title) = upper(?1)")
+    Book findByTitle(String title);
+}
