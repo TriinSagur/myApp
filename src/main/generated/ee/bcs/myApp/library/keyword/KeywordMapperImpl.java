624476045135373e474c7e7a4defe5a326939b15
J00nasKalda <joonask@outlook.com>
2022-04-15 11:11:07 +0300

    .

---
+++ src/main/generated/ee/bcs/myApp/library/keyword/KeywordMapperImpl.java
@@ -0,0 +1,88 @@
+package ee.bcs.myApp.library.keyword;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-04-15T11:09:36+0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.14.1 (JetBrains s.r.o.)"
+)
+@Component
+public class KeywordMapperImpl implements KeywordMapper {
+
+    @Override
+    public Keyword toEntity(KeywordDto keywordDto) {
+        if ( keywordDto == null ) {
+            return null;
+        }
+
+        Keyword keyword = new Keyword();
+
+        keyword.setId( keywordDto.getId() );
+        keyword.setName( keywordDto.getName() );
+
+        return keyword;
+    }
+
+    @Override
+    public KeywordDto toDto(Keyword keyword) {
+        if ( keyword == null ) {
+            return null;
+        }
+
+        Integer id = null;
+        String name = null;
+
+        id = keyword.getId();
+        name = keyword.getName();
+
+        KeywordDto keywordDto = new KeywordDto( id, name );
+
+        return keywordDto;
+    }
+
+    @Override
+    public List<Keyword> toEntities(List<KeywordDto> keywordDto) {
+        if ( keywordDto == null ) {
+            return null;
+        }
+
+        List<Keyword> list = new ArrayList<Keyword>( keywordDto.size() );
+        for ( KeywordDto keywordDto1 : keywordDto ) {
+            list.add( toEntity( keywordDto1 ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<KeywordDto> toDtos(List<Keyword> keyword) {
+        if ( keyword == null ) {
+            return null;
+        }
+
+        List<KeywordDto> list = new ArrayList<KeywordDto>( keyword.size() );
+        for ( Keyword keyword1 : keyword ) {
+            list.add( toDto( keyword1 ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public void updateKeywordFromKeywordDto(KeywordDto keywordDto, Keyword keyword) {
+        if ( keywordDto == null ) {
+            return;
+        }
+
+        if ( keywordDto.getId() != null ) {
+            keyword.setId( keywordDto.getId() );
+        }
+        if ( keywordDto.getName() != null ) {
+            keyword.setName( keywordDto.getName() );
+        }
+    }
+}
