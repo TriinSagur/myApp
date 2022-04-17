a8bccebc60f599ef02d3b5ca944e94eeac05c75a
holgerpart <Hobevalge1>
2022-04-17 09:27:59 +0300

    HOLGER updated

---
+++ src/main/java/ee/bcs/myApp/library/bookout/BookOutMapper.java
@@ -0,0 +1,18 @@
+package ee.bcs.myApp.library.bookout;
+
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
+public interface BookOutMapper {
+    @Mapping(source = "bookId", target = "book.id")
+    @Mapping(source = "bookTitle", target = "book.title")
+    @Mapping(source = "bookYear", target = "book.year")
+    BookOut bookOutDtoToBookOut(BookOutDto bookOutDto);
+
+    @InheritInverseConfiguration(name = "bookOutDtoToBookOut")
+    BookOutDto bookOutToBookOutDto(BookOut bookOut);
+
+    @InheritConfiguration(name = "bookOutDtoToBookOut")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateBookOutFromBookOutDto(BookOutDto bookOutDto, @MappingTarget BookOut bookOut);
+}
