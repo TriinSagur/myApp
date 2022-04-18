b375cd5bef21ec5fefce46ca7c614425fcce5557
KarliEST <karlikullasepp@gmail.com>
2022-04-18 12:12:52 +0300

    Library- lisatud bookauthor sisend author+book Bank - lisatud deposit/credit transaction-d

---
+++ src/main/generated/ee/bcs/myApp/library/book/BookMapperImpl.java
@@ -0,0 +1,76 @@
+package ee.bcs.myApp.library.book;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-04-18T09:32:33+0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
+)
+@Component
+public class BookMapperImpl implements BookMapper {
+
+    @Override
+    public Book bookDtoToBook(BookDto bookDto) {
+        if ( bookDto == null ) {
+            return null;
+        }
+
+        Book book = new Book();
+
+        book.setId( bookDto.getId() );
+        book.setTitle( bookDto.getTitle() );
+        book.setYear( bookDto.getYear() );
+
+        return book;
+    }
+
+    @Override
+    public BookDto bookToBookDto(Book book) {
+        if ( book == null ) {
+            return null;
+        }
+
+        BookDto bookDto = new BookDto();
+
+        bookDto.setId( book.getId() );
+        bookDto.setTitle( book.getTitle() );
+        bookDto.setYear( book.getYear() );
+
+        return bookDto;
+    }
+
+    @Override
+    public List<BookDto> bookToBookDtos(List<Book> books) {
+        if ( books == null ) {
+            return null;
+        }
+
+        List<BookDto> list = new ArrayList<BookDto>( books.size() );
+        for ( Book book : books ) {
+            list.add( bookToBookDto( book ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public void updateBookFromBookDto(BookDto bookDto, Book book) {
+        if ( bookDto == null ) {
+            return;
+        }
+
+        if ( bookDto.getId() != null ) {
+            book.setId( bookDto.getId() );
+        }
+        if ( bookDto.getTitle() != null ) {
+            book.setTitle( bookDto.getTitle() );
+        }
+        if ( bookDto.getYear() != null ) {
+            book.setYear( bookDto.getYear() );
+        }
+    }
+}
--- src/main/java/ee/bcs/myApp/library/author/AuthorDto1.java
+++
@@ -1,9 +0,0 @@
-package ee.bcs.myApp.library.author;
-
-import lombok.Data;
-
-@Data
-public class AuthorDto1 {
-    private String firstName;
-    private String lastName;
-}
