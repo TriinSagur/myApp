0165b35a5469ff51df30391edaa186280897ce85
annvool <annvool@gmail.com>
2022-04-18 16:27:24 +0300

    ANN updates

---
+++ src/main/generated/ee/bcs/myApp/bank/domain/transaction/TransactionMapperImpl.java
@@ -0,0 +1,47 @@
+package ee.bcs.myApp.bank.domain.transaction;
+
+import ee.bcs.myApp.bank.service.DepositRequest;
+import ee.bcs.myApp.bank.service.WithdrawRequest;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-04-18T12:01:33+0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+)
+@Component
+public class TransactionMapperImpl implements TransactionMapper {
+
+    @Override
+    public Transaction toDepositEntity(DepositRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        Transaction transaction = new Transaction();
+
+        transaction.setAmount( request.getAmount() );
+
+        transaction.setSenderAccountNumber( "ATM" );
+        transaction.setType( "d" );
+
+        return transaction;
+    }
+
+    @Override
+    public Transaction toWithdrawEntity(WithdrawRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        Transaction transaction = new Transaction();
+
+        transaction.setAmount( request.getAmount() );
+
+        transaction.setReceiverAccountNumber( "ATM" );
+        transaction.setType( "w" );
+
+        return transaction;
+    }
+}
