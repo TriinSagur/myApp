9412c2a31a0eba338db40e6e79d2f4f92ad38228
500 <git.hub@myrk.eu>
2022-04-19 19:23:16 +0300

    JAAN web - HTML, CSS

--- src/main/generated/ee/bcs/myApp/library/bookauthor/BookAuthorMapperImpl.java
+++ src/main/generated/ee/bcs/myApp/library/bookauthor/BookAuthorMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-04-19T12:11:30+0300",
+    date = "2022-04-19T13:27:26+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.14.1 (Amazon.com Inc.)"
 )
 @Component
