7ed8d5ea8831ccf00b6bdec1153aa06bd52da7ba
Rain Tüür <rain.tuur@gmail.com>
2022-04-19 11:24:30 +0300

    RAIN update

--- src/main/java/ee/bcs/myApp/validation/ValidationService.java
+++ src/main/java/ee/bcs/myApp/validation/ValidationService.java
@@ -1,6 +1,7 @@
 package ee.bcs.myApp.validation;
 
 import ee.bcs.myApp.bank.domain.account.Account;
+import ee.bcs.myApp.infrastructure.exception.BusinessException;
 import ee.bcs.myApp.infrastructure.exception.DataNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +12,19 @@ public class ValidationService {
 
 
     public static final String ACCOUNT_NOT_EXISTS = "Sellist kontot ei eksisteeri";
+    public static final String DEPOSIT_OVER_LIMIT = "Deposiidi limiit on ületatud";
 
     public void accountExists(Integer accountId, Optional<Account> account) {
         if (account.isEmpty()) {
             throw new DataNotFoundException(ACCOUNT_NOT_EXISTS, "Kontot ID'ga " + accountId + " ei leitud");
+        }
+    }
+
+    public void isValidDepositAmount(Integer amount) {
+        // productionsis tuleks see väärtus andmebaasist
+        Integer limit = 5000;
+        if (amount > limit) {
+            throw new BusinessException(DEPOSIT_OVER_LIMIT, "Summa €" + amount + " ületab limiidi €" + limit);
         }
     }
 }
