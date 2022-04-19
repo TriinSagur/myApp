62430a8cfedc0a45b4b5e2abb226261926977a35
KarliEST <karlikullasepp@gmail.com>
2022-04-19 16:40:02 +0300

    Bank - veateadete lisamine

--- src/main/generated/ee/bcs/myApp/library/book/BookMapperImpl.java
+++ src/main/generated/ee/bcs/myApp/library/book/BookMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-04-19T12:11:07+0300",
+    date = "2022-04-19T13:27:56+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
 )
 @Component
