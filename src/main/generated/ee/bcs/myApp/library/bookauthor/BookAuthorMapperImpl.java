51b47be7f795582b4f1831879f4c6b304bfb20bb
kasparpoldemaa <kasparpoldemaa@gmail.com>
2022-04-19 21:11:36 +0300

    updated

--- src/main/generated/ee/bcs/myApp/library/bookauthor/BookAuthorMapperImpl.java
+++ src/main/generated/ee/bcs/myApp/library/bookauthor/BookAuthorMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-04-19T11:23:40+0300",
+    date = "2022-04-19T13:27:19+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.14.1 (Amazon.com Inc.)"
 )
 @Component
