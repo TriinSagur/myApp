f71c0387c382af2778e9d29e59c1140704cc868c
500 <git.hub@myrk.eu>
2022-04-21 09:51:31 +0300

    JAAN JavaScript harjutuse lõpetus.

--- src/main/java/ee/bcs/myApp/bank/domain/customer/CustomerDto.java
+++ src/main/java/ee/bcs/myApp/bank/domain/customer/CustomerDto.java
@@ -16,7 +16,7 @@ public class CustomerDto {
     private String lastName;
 
     @NotNull
-    @Size(min = 11, max = 11)
+    @Size(max = 11)
     private String isikukood;
 }
 
--- src/main/java/ee/bcs/myApp/bank/domain/customer/CustomerService.java
+++ src/main/java/ee/bcs/myApp/bank/domain/customer/CustomerService.java
@@ -1,9 +1,11 @@
 package ee.bcs.myApp.bank.domain.customer;
 
+import ee.bcs.myApp.validation.ValidationService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -14,14 +16,14 @@ public class CustomerService {
     @Resource
     private CustomerRepository customerRepository;
 
+    @Resource
+    private ValidationService validationService;
+
     public CustomerDto addNewCustomer(CustomerDto customerDto) {
         Customer customer = customerMapper.toEntity(customerDto);
         customerRepository.save(customer);
-
-
         return customerMapper.toDto(customer);
     }
-
 
     public List<CustomerDto> getAllCustomers() {
         List<Customer> allCustomers = customerRepository.findAll();
@@ -29,20 +31,25 @@ public class CustomerService {
     }
 
     public CustomerDto findCustomerById(Integer id) {
-        // TODO: kontroll kas customer on olemas. Veateade!
-        Customer customer = customerRepository.getById(id);
-        return  customerMapper.toDto(customer);
+        Customer customer = getValidCustomerById(id);
+        return customerMapper.toDto(customer);
     }
 
-    public void removeCustomerById(Integer id) {
-        // TODO: kontroll kas customer on olemas. Veateade!
-        customerRepository.deleteById(id);
+    public void removeCustomerById(Integer customerId) {
+        Customer customer = getValidCustomerById(customerId);
+        customerRepository.deleteById(customer.getId());
     }
 
     public void updateCustomerById(Integer customerId, CustomerDto customerDto) {
-
         Customer customer = customerRepository.getById(customerId);
         customerMapper.updateEntity(customerDto, customer);
         customerRepository.save(customer);
     }
+
+    private Customer getValidCustomerById(Integer customerId) {
+        Optional<Customer> customer = customerRepository.findById(customerId);
+        validationService.customerExists(customerId, customer);
+        return customer.get();
+    }
+
 }
