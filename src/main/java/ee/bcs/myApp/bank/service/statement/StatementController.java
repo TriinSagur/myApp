605cdbcb44336a624ceb09135d5bea1276e5fc56
Rain Tüür <rain.tuur@gmail.com>
2022-04-22 12:17:14 +0300

    RAIN update

--- src/main/java/ee/bcs/myApp/bank/service/statement/StatementController.java
+++ src/main/java/ee/bcs/myApp/bank/service/statement/StatementController.java
@@ -1,5 +1,6 @@
 package ee.bcs.myApp.bank.service.statement;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ public class StatementController {
     private StatementService statementService;
 
     @GetMapping("/account-id")
+    @Operation(summary = "Leiab kontoväljavõtte konto ID järgi")
     public StatementResponse getStatementByAccountId(@RequestParam Integer accountId) {
         return statementService.getStatementByAccountId(accountId);
     }
