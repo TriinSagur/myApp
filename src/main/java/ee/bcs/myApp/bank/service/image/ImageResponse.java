d3a9f476214a4f46edf03e9d40266e56cf56ffd9
marjutamm <tamm.marju@gmail.com>
2022-04-25 14:52:31 +0300

    MARJU created some new endpoints

--- src/main/java/ee/bcs/myApp/bank/service/image/ImageResponse.java
+++ src/main/java/ee/bcs/myApp/bank/service/image/ImageResponse.java
@@ -3,7 +3,7 @@ package ee.bcs.myApp.bank.service.image;
 import lombok.Data;
 
 @Data
-public class ImageRequest {
+public class ImageResponse {
     private Integer customerId;
     private String title;
     private String description;
