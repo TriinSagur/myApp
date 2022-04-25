0a1c5f4782f67b0160d17224803f88f096906b22
sergeijagur <sergei.jagur@gmail.com>
2022-04-25 15:03:09 +0300

    SERGEI  update

--- src/main/java/ee/bcs/myApp/bank/service/image/ImageController.java
+++ src/main/java/ee/bcs/myApp/bank/service/image/ImageController.java
@@ -1,11 +1,9 @@
 package ee.bcs.myApp.bank.service.image;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/picture")
@@ -17,5 +15,10 @@ public class ImageController {
     @PostMapping("in")
     public void addPicture (@RequestBody ImageRequest request) {
         imageService.addPicture(request);
+    }
+
+    @GetMapping("/all")
+    public List< ImageResponse> getAllPictures () {
+        return imageService.getAllPictures();
     }
 }
