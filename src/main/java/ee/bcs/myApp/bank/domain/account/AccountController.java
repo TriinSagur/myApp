2ec39667a26894c6ff41578c574c07cfdef8e007
TriinSagur <triin.sagur@hotmail.com>
2022-04-25 09:01:24 +0300

    TRIIN created some new endpoints

--- src/main/java/ee/bcs/myApp/bank/domain/account/AccountController.java
+++ src/main/java/ee/bcs/myApp/bank/domain/account/AccountController.java
@@ -31,6 +31,12 @@ public class AccountController {
         return accountService.findAccountInfoById(id);
     }
 
+    @GetMapping("/customer-id")
+    @Operation(summary = "Otsib kontode info kliendi ID järgi")
+    public List<AccountResponse> findAccountsInfoByCustomerId(@RequestParam Integer id) {
+        return accountService.findAccountsInfoByCustomerId(id);
+    }
+
     @DeleteMapping("/id")
     public void removeAccountById(@RequestParam Integer id) {
         accountService.removeAccountById(id);
