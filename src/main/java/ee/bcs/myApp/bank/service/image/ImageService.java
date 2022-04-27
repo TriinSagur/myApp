1742ad81bbade55930fcdc4b653c179350f59f8c
marjutamm <tamm.marju@gmail.com>
2022-04-27 11:25:08 +0300

    MARJU created some new endpoints

--- src/main/java/ee/bcs/myApp/bank/service/image/ImageService.java
+++ src/main/java/ee/bcs/myApp/bank/service/image/ImageService.java
@@ -1,10 +1,10 @@
 package ee.bcs.myApp.bank.service.image;
 
-
 import ee.bcs.myApp.bank.domain.picture.PictureService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class ImageService {
@@ -14,6 +14,10 @@ public class ImageService {
 
     public void addPicture(ImageRequest request) {
         pictureService.addPicture(request);
+    }
+
+    public List<ImageResponse> getAllPictures() {
+        return pictureService.getAllPictures();
 
     }
 }
