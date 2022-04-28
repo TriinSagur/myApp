ff21f5592f92ad60e494b56ae11a552043e2e44d
kyllilum <kyllilumiste@gmail.com>
2022-04-28 16:22:56 +0300

    KÜLLI created some new endpoints

--- src/main/java/ee/bcs/myApp/bank/service/transfer/TransferService.java
+++ src/main/java/ee/bcs/myApp/bank/service/transfer/TransferService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 
 @Service
-public class TransferService {
+public class    TransferService {
 
     @Resource
     private AccountService accountService;
